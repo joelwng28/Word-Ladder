8e6b13c2a3d194e65fc3bd01511ac68803b68476
xxuil <liuxxggwp@utexas.edu>
2017-06-21 14:36:14 -0500

    BFS method updated

--- Main_xx.java
+++ Main_xx.java
@@ -117,12 +117,99 @@ public class Main_xx {
 	
     public static ArrayList<String> getWordLadderBFS(String start, String end) {
 		
-		// TODO some code
+    	ArrayList<String> newList = new ArrayList<String>();
+
+    	//Check the length first
+    	if(start.length() != end.length()){
+    		newList.add("INVALID INPUT");
+    		return newList;
+    	}
+
+    	//Check if there is a word in the input string
+    	if(start.length() == 0){
+    		newList.add("INVALID INPUT");
+    		return newList;
+    	}
+
+    	//Setup dictionary
 		Set<String> dict = makeDictionary();
-		// TODO more code
+		
+		if(dict.isEmpty()){
+			newList.add("INVALID DICT");
+    		return newList;
+		}
+		
+		//Setup two queues for BFS
+		Queue<String> push = new LinkedList<String>();
+		Queue<String> pop = new LinkedList<String>();
+
+		pop.add(start);
+		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		
+		while(dict.size() > 0 && !pop.isEmpty()){
+			boolean flag = false;
+			while(!pop.isEmpty() && !flag){
+				String str = pop.peek();
+				//The execution bound of this for loop should be the length of start
+				for(int i = 0; i < str.length() && !flag; i++){
+					//The execution bound of this for loop should be 26
+					for(int j = 0; j < alphabet.length; j++){
+						
+						if(str.charAt(i) == alphabet[j]){
+							//go to next iteration
+							continue;
+						}
+						
+						//Create the temporary word 
+						//The temp word differs the original word by one character
+						char[] tempChar = str.toCharArray();
+						tempChar[i] = alphabet[j];
+						String tempString = String.valueOf(tempChar);
+						
+						//Check if the Ladder has reach to the end
+						if(tempString.equals(end)){
+							//The ladder has been found
+							//return
+							flag = true;
+							break;
+						}
+						
+						//Check if the dict contains the word
+						if(dict.contains(tempString)){
+							//the word is in dict
+							//push it into the queue and delete it from dict
+							push.add(tempString);
+							dict.remove(tempString);
+						}
+					}
+				}
+				if(!flag){
+					pop.poll();
+				}
+			}
+			
+			if(flag){
+				
+			}
+			else{
+				pop = swap(push);
+			}
+		}
+
 		
 		return null; // replace this line later with real return
 	}
+    
+    private static Queue<String> swap(Queue<String> input){
+    	Queue<String> res = new LinkedList<String>();
+    	while(!input.isEmpty()){
+    		res.add(input.poll());
+    	}
+    	input.clear();
+    	return res;
+    }
+    
+    
     
 	
 	public static void printLadder(ArrayList<String> ladder) {
@@ -130,7 +217,6 @@ public class Main_xx {
 	}
 	// TODO
 	// Other private static methods here
-
 
 	/* Do not modify makeDictionary */
 	public static Set<String>  makeDictionary () {
