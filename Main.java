eb801ab9ff596a3c0d617b4ab695c6aec800ad1c
joelwng28 <joelwng28@gmail.com>
2017-06-21 22:19:40 -0500

    finished DFS

--- Main.java
+++ Main.java
@@ -17,19 +17,11 @@ package assignment3;
 import java.util.*;
 import java.io.*;
 
-<<<<<<< HEAD
+
 public class Main {
 	public static final boolean DEBUG = false;
 	// static variables and constants only here.
 	public static ArrayList<String> dictionary;
-	public static ArrayList<ArrayList<String>> edges;
-=======
-public class Main_xx {
-	
-	public static final boolean DEBUG = true;
-	
-	// static variables and constants only here.
->>>>>>> 8e6b13c2a3d194e65fc3bd01511ac68803b68476
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -45,43 +37,55 @@ public class Main_xx {
 			ps = System.out;			// default output to Stdout
 		}
 		initialize();
-<<<<<<< HEAD
 		//ArrayList<String> list = parse(kb);
-		fillEdges();
-		System.out.println(edges);
 		//printLadder(list);
+		System.out.println(getWordLadderDFS("HANTS", "WARES"));
 	}
+	
+	public static ArrayList<String> getWordLadderDFS(String start, String end) {
+		
+		// Returned list should be ordered start to end.  Include start and end.
+		// If ladder is empty, return list with just start and end.
+		ArrayList<String> answer = new ArrayList<String>();
+		ArrayList<String> edges = fillEdges(start);
+		if(edges.contains(end)){
+			answer.add(start);
+			answer.add(end);
+			initialize();
+			return answer;
+		}
+		dictionary.remove(start);
+		while(!edges.isEmpty()){
+			ArrayList<String> temp = getWordLadderDFS(edges.get(0), end);
+			if(temp != null){
+				answer.add(start);
+				answer.addAll(temp);
+				return answer;
+			}
+			edges.remove(0);
+		}
+		return null;
+	}
+	
 	
 	public static void initialize() {
 		Set<String> dict = makeDictionary();
 		dictionary = new ArrayList<String>(dict);
-		edges = new ArrayList<ArrayList<String>>();
 	}
 	
-	public static void fillEdges(){
+	public static ArrayList<String> fillEdges(String str){
+		ArrayList<String> temp = new ArrayList<String>();
 		for(int i = 0; i < dictionary.size(); i++){
-			ArrayList<String> temp = new ArrayList<String>();
-			for(int j = 0; j < 5; j++){
-				for(int k = (i + 1); k < dictionary.size(); k++){
-					if(dictionary.get(i).substring(0,j).equals(dictionary.get(k).substring(0,j)) && dictionary.get(i).substring((j+1),5).equals(dictionary.get(k).substring((j+1),5))){
-						temp.add(dictionary.get(k));
+			if(!str.equals(dictionary.get(i))){
+				for(int j = 0; j < 5; j++){
+					if(str.substring(0,j).equals(dictionary.get(i).substring(0,j)) && str.substring((j+1),5).equals(dictionary.get(i).substring((j+1),5))){
+						temp.add(dictionary.get(i));
 					}
 				}
 			}
-			edges.add(temp);
 		}
-=======
-		
-		// TODO methods to read in words, output ladder
+		return temp;
 	}
-	
-	public static void initialize() {
-		// initialize your static variables or constants here.
-		// We will call this method before running our JUNIT tests.  So call it 
-		// only once at the start of main.
->>>>>>> 8e6b13c2a3d194e65fc3bd01511ac68803b68476
-	}
-	
 	/**Parse method
 	 * This method takes input from keyboard and determine if the input is valid
 	 * @param keyboard Scanner connected to System.in
@@ -118,86 +122,34 @@ public class Main_xx {
 				}
 				
 				if(count == 1){
-<<<<<<< HEAD
 					if(inputList.get(0).toLowerCase().equals("/quit")){
 						lineScan.close();
-=======
-					if(inputList.get(0).toLowerCase().equals("quit")){
->>>>>>> 8e6b13c2a3d194e65fc3bd01511ac68803b68476
+					if(inputList.get(0).toLowerCase().equals("quit"))
 						break;
 					}
 					else{
 						inputList.set(0, "INVALID");
-<<<<<<< HEAD
 						lineScan.close();
-=======
->>>>>>> 8e6b13c2a3d194e65fc3bd01511ac68803b68476
 						break;
 					}
 				}
 				if(count >2){
 					inputList.set(0, "INVALID");
-<<<<<<< HEAD
 					lineScan.close();
-=======
->>>>>>> 8e6b13c2a3d194e65fc3bd01511ac68803b68476
 					break;
 				}
-				
 			}
 			
 			if(count == 2)
-<<<<<<< HEAD
 				lineScan.close();
-=======
->>>>>>> 8e6b13c2a3d194e65fc3bd01511ac68803b68476
 				break;
 		}
 		return inputList;
 	}
 	
-	public static ArrayList<String> getWordLadderDFS(String start, String end) {
-		
-		// Returned list should be ordered start to end.  Include start and end.
-		// If ladder is empty, return list with just start and end.
-<<<<<<< HEAD
-		ArrayList<String> temp = new ArrayList<String>();
-		for(int i = 0; i < 5; i++){
-			if(start.substring(0,i).equals(end.substring(0,i)) && start.substring((i+1),5).equals(end.substring((i+1),5))){
-				temp.add(start);
-				temp.add(end);
-			}
-		}
-		return temp;
-=======
-		// TODO some code
-		Set<String> dict = makeDictionary();
-		// TODO more code
-		
-		return null; // replace this line later with real return
->>>>>>> 8e6b13c2a3d194e65fc3bd01511ac68803b68476
-	}
-	
     public static ArrayList<String> getWordLadderBFS(String start, String end) {
 
-    	ArrayList<String> newList = new Arraylist<String>();
-
-    	//Check the length first
-    	if(start.length != end.length){
-    		newList.add("INVALID");
-    		return newList;
-    	}
-
-    	//Check if there is a word in the input string
-    	if(start.length == 0){
-    		newList.add("INVALID");
-    		return newList;
-    	}
-
-    	//Setup dictionary
-		Set<String> dict = makeDictionary();
-
-		
+    	
 		return null; // replace this line later with real return
 	}
     
@@ -216,7 +168,7 @@ public class Main_xx {
 		Set<String> words = new HashSet<String>();
 		Scanner infile = null;
 		try {
-			infile = new Scanner (new File("short_dict.txt"));
+			infile = new Scanner (new File("five_letter_words.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("Dictionary File not Found!");
 			e.printStackTrace();
